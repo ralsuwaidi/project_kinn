17acac1186a4847a6c042747534d251138575b08
real4sight <x_suwaidi@hotmail.com>
2018-03-01 11:19:39 +0400

    can count text in file and read txt file

--- user_write.py
+++ user_write.py
@@ -2,16 +2,12 @@ import textwrap
 from text_1 import *
 from functions_1 import *
 import re
+import sys
+count = 0
+for line in open('text_test.txt', 'r'):
+    print line
+    if "[c]" in line:
+        count+=1
 
-print "Enter/Paste your content. Ctrl-D to save it."
-contents = []
-while True:
-    try:
-        line = raw_input("")
-    except EOFError:
-        break
-    contents.append(line)
-
-print "you typed:", contents
-
+print count
 v=raw_input("")
